59fefc42e9d3a2bdfb3e71cd7ec751b9eca79411
Sebastian_Velasquez <vesebastian9@gmail.com>
2026-05-07 23:54:46 -0500

    Refactor OracleDBConection to use dotenv for config

--- src/main/java/com/mycompany/util/OracleDBConection.java
+++ src/main/java/com/mycompany/util/OracleDBConection.java
@@ -1,32 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.util;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author dvd - steven sanchez
- */
 public class OracleDBConection {
-    private static final String URL = "jdbc:oracle:thin:@orion.javeriana.edu.co:1521/lab";
-    private static final String USER = "is213820";
-    private static final String PASSWORD = "U4WIYQmjpLrysAH";
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String URL = dotenv.get("DB_URL");
+    private static final String USER = dotenv.get("DB_USER");
+    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
 
     public Connection getConnection() {
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Conexión exitosa a MySQL");
+            System.out.println("Conexión exitosa a Oracle");
         } catch (SQLException e) {
             System.out.println("Error en la conexión: " + e.getMessage());
         }
         return connection;
     }
-
-   
 }
